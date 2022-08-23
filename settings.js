ad54cc899a106bca4e1579c25e005a525cca2f36
mikasaBotz456 <108715025+mikasaBotz456@users.noreply.github.com>
2022-08-23 17:35:03 +0700

    update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIKeys = {
 // setting 
 global.autoread = false // auto read message
 global.autobio = true 
-global.userRegister = false
+global.userRegister = true
 
 //language
 //Available in indonesia , english & spanyol 
